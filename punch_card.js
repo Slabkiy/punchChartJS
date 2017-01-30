6e76f3123cb6dc169c64feb4c15336aaca67368c
slabkiy <slabkiya@gmail.com>
2017-01-30 14:25:40 +0300

    0.3 стиль punchChart

--- punch_card.js
+++ punch_card.js
@@ -5,7 +5,7 @@
 
 
 
-
+'use strict';
 window.addEventListener('resize', () => {
     if( document.getElementById('c_pc') !== undefined ){
         document.getElementById('c_pc').remove()
@@ -15,15 +15,16 @@ window.addEventListener('resize', () => {
     }
     punch_card.create();
 });
-'use strict';
 class PunchCard {
     constructor( settings ){
         this.where = settings.where;
         this.what = settings.what;
         this.height = settings.height;
+        this.label = settings.label;
+        this.color = settings.color;
     }
     create(){
-        var circle_position = [];
+        var circle_data = [];
 
         /*Создаем канвасы*/
 
@@ -54,25 +55,44 @@ class PunchCard {
         }else{
             var max_radius = parseInt((padding_x-8)/2);
         }
-
+        var color = this.color;
         var min_radius = 2;
         var chart = canv_punch.getContext('2d');
         var popup_c = canv_popup.getContext('2d');
-        var label = [ 'Понедельник', "Вторник", "Среда" ,"Четверг", "Пятница", "Суббота", 'Воскресенье'];
+        var label = this.label;
         draw_punch_card();
         punch_card.onmousemove = function(e){
 
             popup_c.clearRect(0,0, canv_popup.width, canv_popup.height);
-           for( var i = 0; i < circle_position.length; i++ ){
+           for( var i = 0; i < circle_data.length; i++ ){
 
-              if( e.layerX >= circle_position[i].x-max_radius && e.layerX <= circle_position[i].x+max_radius && e.layerY >= circle_position[i].y-max_radius && e.layerY <= circle_position[i].y+max_radius ){
+              if( e.layerX >= circle_data[i].x-max_radius && e.layerX <= circle_data[i].x+max_radius && e.layerY >= circle_data[i].y-max_radius && e.layerY <= circle_data[i].y+max_radius ){
 
                   popup_c.beginPath();
-                  popup_c.fillStyle = 'rgba( 78, 205, 196, 0.4 )';
-                  popup_c.fillRect(0, circle_position[i].y-(padding_y/2), x, padding_y);
-                  popup_c.font = "9px halflings, 'sans-serif'";
-                  popup_c.fillStyle = 'rgb(44, 62, 80)';
-                  popup_c.fillText(circle_position[i].text, circle_position[i].x-(max_radius/2), circle_position[i].y-( padding_y/3 ));
+                  popup_c.fillStyle = 'rgba( 227, 242, 253, 0.6 )';
+                  popup_c.fillRect(0, circle_data[i].y-(padding_y/2), x+max_radius, padding_y);
+                  popup_c.fillStyle = 'rgba( 96, 125, 139, 0.8 )';
+                  popup_c.arc( circle_data[i].x, circle_data[i].y, max_radius-5, 0, 2 * Math.PI, false );
+                  popup_c.fill();
+                  popup_c.font = "9px verdana, 'sans-serif'";
+                  popup_c.fillStyle = 'rgb(255, 255, 255)';
+                  switch (circle_data[i].text.toString().length) {
+                    case 1:
+                        popup_c.fillText(circle_data[i].text, circle_data[i].x-(max_radius/6), circle_data[i].y+3);
+                      break;
+                    case 2:
+                        popup_c.fillText(circle_data[i].text, circle_data[i].x-(max_radius/3.5), circle_data[i].y+3);
+                      break;
+                    case 3:
+                        popup_c.fillText(circle_data[i].text, circle_data[i].x-(max_radius/2), circle_data[i].y+3);
+                      break;
+                    case 4:
+                        popup_c.fillText(circle_data[i].text, circle_data[i].x-(max_radius/1.5), circle_data[i].y+3);
+                      break;
+                    default:
+                      popup_c.fillText(circle_data[i].text, circle_data[i].x-(max_radius/1.3), circle_data[i].y+3);
+
+                  }
 
               }
 
@@ -90,15 +110,15 @@ class PunchCard {
 
                     for( var j = 0; j < result_array[i].length; j++ ){
                         if( i == Object.keys(result_array).length){
-                            chart.font = "9px halflings, 'sans-serif'";
+                            chart.font = "9px verdana, 'sans-serif'";
                             chart.fillText(j+'ч' ,padding_start_x+padding_x*(j+1), padding_y*(i)+9);
 
                         }
                         chart.beginPath();
                         chart.moveTo(padding_start_x+padding_x*(j+1), padding_y*(i));
-                        chart.lineWidth = 0.5;
+                        chart.lineWidth = 0.1;
                         chart.lineTo(padding_start_x+padding_x*(j+1), padding_y*(i)-6);
-                        chart.strokeStyle = "rgb(108 , 122, 137)";
+                        chart.strokeStyle = "rgb(63 , 81, 181)";
                         chart.stroke();
                         chart.closePath();
 
@@ -106,12 +126,12 @@ class PunchCard {
 
                         chart.beginPath();
                         chart.moveTo(0, padding_y*(i));
-                        chart.font = "12px halflings, 'sans-serif'";
+                        chart.font = "12px verdana, 'sans-serif'";
                         chart.fillStyle = "rgb(44, 62, 80)";
                         chart.fillText(label[i-1], 0, padding_y*(i)-(padding_y/2.5));
-                        chart.lineWidth = 0.5;
-                        chart.lineTo(x, padding_y*(i));
-                        chart.strokeStyle = "rgba(108 , 122, 137)";
+                        chart.lineWidth = 0.1;
+                        chart.lineTo(x+max_radius, padding_y*(i));
+                        chart.strokeStyle = "rgba(63 , 81, 181)";
                         chart.stroke();
                         chart.closePath();
 
@@ -126,16 +146,17 @@ class PunchCard {
 
                             chart.beginPath();
                             chart.arc( padding_start_x+padding_x*(j+1), padding_y*(i)-(padding_y/2), set_radius(result_array[i][j]), 0, 2*Math.PI, false);
-                            chart.fillStyle = "rgb(0," + Math.floor(255-35.5*i)+","+Math.floor(255-10.5*j)+")"; // сделать цвет в зависиости от количества действий
-                            chart.strokeStyle = "rgb(0," + Math.floor(255-35.5*i)+","+Math.floor(255-10.5*j)+")"; //
+                            chart.fillStyle = color;
+                            chart.strokeStyle = color;
                             chart.fill();
                             chart.stroke();
                             var obj = {
                                 'x' : padding_start_x+padding_x*(j+1),
                                 'y' : padding_y*(i)-(padding_y/2),
-                                'text': result_array[i][j]
+                                'text': result_array[i][j],
+                                'radius': set_radius(result_array[i][j])
                             }
-                            circle_position.push(obj);
+                            circle_data.push(obj);
                         }
                     }
 
